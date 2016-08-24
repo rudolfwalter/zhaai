177e3757bc7458db7a28b652b5b5168248c04e3c
Rudolf-Walter Kiss-Szakacs <sk_RW@yahoo.com>
2016-08-24 19:03:05 +0300

    Add ability to define fixed ints on the command line.

--- util.h
+++ util.h
@@ -10,14 +10,30 @@
 
 #define GS_ASSERT(cond) enum { PP_CONCAT(gs_assert_line_, __LINE__) = 1/(!!(cond)) }
 
+#ifndef int8_t
 typedef signed   char       int8_t;
+#endif
+#ifndef uint8_t
 typedef unsigned char      uint8_t;
+#endif
+#ifndef int16_t
 typedef signed   short      int16_t;
+#endif
+#ifndef uint16_t
 typedef unsigned short     uint16_t;
+#endif
+#ifndef int32_t
 typedef signed   int        int32_t;
+#endif
+#ifndef uint32_t
 typedef unsigned int       uint32_t;
+#endif
+#ifndef int64_t
 typedef ptrdiff_t           int64_t;
+#endif
+#ifndef uint64_t
 typedef size_t             uint64_t;
+#endif
 
 GS_ASSERT(sizeof( int8_t ) == 1);
 GS_ASSERT(sizeof(uint8_t ) == 1);
@@ -29,3 +45,4 @@ GS_ASSERT(sizeof( int64_t) == 8);
 GS_ASSERT(sizeof(uint64_t) == 8);
 
 #endif
+
